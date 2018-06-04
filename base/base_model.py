654a9ad4ea6768c1473ed271a2c6f2ef509b195b
yangshuai <yangshuai@pachiratech.com>
2018-06-04 10:07:09 +0800

    Set some tf.Variables untrainable.

--- base/base_model.py
+++ base/base_model.py
@@ -47,14 +47,14 @@ class BaseModel:
     # just initialize a tensorflow variable to use it as epoch counter
     def init_cur_epoch(self):
         with tf.variable_scope('cur_epoch'):
-            self.cur_epoch_tensor = tf.Variable(0, trainable=True, name='cur_epoch')
+            self.cur_epoch_tensor = tf.Variable(0, trainable=False, name='cur_epoch')
             self.increment_cur_epoch_tensor = tf.assign_add(self.cur_epoch_tensor,1)
 
     # just initialize a tensorflow variable to use it as global step counter
     def init_global_step(self):
         # DON'T forget to add the global step tensor to the tensorflow trainer
         with tf.variable_scope('global_step'):
-            self.global_step_tensor = tf.Variable(0, trainable=True, name='global_step')
+            self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
 
     def init_saver(self):
         # just copy the following line in your child class
